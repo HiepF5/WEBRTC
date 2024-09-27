12c2209d200972a3b0b3afb388634caff92c0b2c
Nguyen Cong Hiep <hiepf5.nguyencong.ptit@gmail.com>
2024-09-27 14:21:05 +0700

    v2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,27 @@
-"use strict";
-console.log("running server");
+import express from "express";
+
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+const port = 8080;
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
